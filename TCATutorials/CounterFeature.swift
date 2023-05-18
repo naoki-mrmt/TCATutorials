43ffbbed04b8a438defa5d985441ea71ba6cd4ad
naoki-mrmt <n.bakupu@gmail.com>
2023-05-18 21:50:28 +0900

    Integrating with SwiftUI

--- TCATutorials/CounterFeature.swift
+++ TCATutorials/CounterFeature.swift
@@ -6,6 +6,7 @@
 //
 
 import ComposableArchitecture
+import SwiftUI
 
 struct CounterFeature: ReducerProtocol {
     // MARK: - State
@@ -29,5 +30,56 @@ struct CounterFeature: ReducerProtocol {
             state.count += 1
             return .none
         }
+    }
+}
+
+extension CounterFeature.State: Equatable {}
+
+
+struct CounterView: View {
+    // MARK: - store
+    let store: StoreOf<CounterFeature>
+
+    // MARK: - Body
+    var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            VStack {
+                Text("\(viewStore.count)")
+                    .font(.largeTitle)
+                    .padding()
+                    .background(.black.opacity(0.1))
+                    .cornerRadius(8)
+                HStack {
+                    Button {
+                        viewStore.send(.tappedDecrementButton)
+                    } label: {
+                        Text("-")
+                            .font(.largeTitle)
+                            .padding()
+                    } //: Button
+                    .background(.black.opacity(0.1))
+                    .cornerRadius(8)
+                    Button {
+                        viewStore.send(.tappedIncrementButton)
+                    } label: {
+                        Text("+")
+                            .font(.largeTitle)
+                            .padding()
+                    } //: Button
+                    .background(.black.opacity(0.1))
+                    .cornerRadius(8)
+                } //: HStack
+            } //: VStack
+        } //: WithViewStore
+    }
+}
+
+struct CounterPreview: PreviewProvider {
+    static var previews: some View {
+        CounterView(
+            store: Store(initialState: CounterFeature.State()) {
+                CounterFeature()
+            }
+        )
     }
 }
